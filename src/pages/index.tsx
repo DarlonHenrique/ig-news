4974cfdf5fb061ef660831ecab1fe42700e52dcb
Darlon Henrique <darlonhenrisouza@gmail.com>
2022-06-08 09:47:24 -0300

    feat: create home page and her style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,26 @@
+import Head from 'next/head'
+import styles from './home.module.scss'
+
 export default function Home() {
-  return <h1>Hello World of Next JS</h1>
+  return (
+    <>
+      <Head>
+        <title>Home | Ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome </span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            get acess to all the publications <br />
+            <span>for $9.99 month</span>
+          </p>
+        </section>
+
+        <img src='/images/avatar.svg' alt='girl coding' />
+      </main>
+    </>
+  )
 }
