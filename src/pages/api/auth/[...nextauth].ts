dd38b79e78f3ea15b8fee6118d1a7c762e1c3556
Darlon Henrique <darlonhenrisouza@gmail.com>
2022-06-13 17:26:14 -0300

    feat(authentication): create signIn callback

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { query } from 'faunadb'
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -7,5 +9,19 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          query.Create(query.Collection('users'), { data: { email } })
+        )
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
